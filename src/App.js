aad271ba8d82b1f6084f31b7f2eb7f73ed9e7223
thienJs <cristian.albarran95@gmail.com>
2018-12-07 15:04:49 -0400

    starter folder structure

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from 'react';
+import Layout from './components/Layout/Layout';
+
+class App extends Component {
+  
+  render() {
+   
+    return (
+      <Fragment>
+        <Layout>
+          Test
+        </Layout>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
