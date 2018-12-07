6084960d296d06ad1d5d7eff7f480f11d6a971a1
thienJs <cristian.albarran95@gmail.com>
2018-12-07 19:44:28 -0400

    layout.module.css added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,14 @@
 import React, { Fragment } from 'react';
 
+import classes from './Layout.module.css';
+
 const layout = ( props ) => (
+  
   <Fragment>
     <div>
       tollbar, sideNav, Backdrop
     </div>
-    <main>
+    <main className={classes.Main}>
         { props.children }
     </main>
   </Fragment>
