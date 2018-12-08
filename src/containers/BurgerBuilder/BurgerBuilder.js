d19fdb3910fe1ceeab8dac21e5b047f8ad4b78dc
thienJs <cristian.albarran95@gmail.com>
2018-12-08 13:35:31 -0400

    display burger ingredients dymacly with state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,8 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
-  render() {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
+  }
 
+  render() {  
+    return (
+      <Fragment>
+        <Burger ingredients={this.state.ingredients}/>
+        <div>Build Control</div>
+      </Fragment>
+    );
+   
   }
 }
 
