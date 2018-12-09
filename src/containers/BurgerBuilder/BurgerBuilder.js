d0afe499a5d95fd0f4dddc7368e8599c8ac8d153
thienJs <cristian.albarran95@gmail.com>
2018-12-09 13:07:34 -0400

    display price for each ingredient

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -63,7 +63,8 @@ class BurgerBuilder extends Component {
         <BuildControls 
           addIngredient={this.addIngredientHandler}
           removeIngredient={this.removeIngredientHandler}
-          disabled={disabledInfo}/>
+          disabled={disabledInfo}
+          price={this.state.totalPrice}/>
       </Fragment>
     );
    
