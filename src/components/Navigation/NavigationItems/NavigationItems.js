6eeb5461a85469851fd7ccfd660d706d308beddd
thienJs <cristian.albarran95@gmail.com>
2019-01-02 22:12:37 -0400

    implementing react-router

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,12 +4,10 @@ import classes from './NavigationItems.module.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = () => (
-  <ul className={classes.NavigationItems}>  
-    <NavigationItem link='/' active >Burger Builder</NavigationItem>
-    <NavigationItem link='/'>Checkout</NavigationItem>
+  <ul className={classes.NavigationItems}>
+    <NavigationItem link="/burger-builder">Burger Builder</NavigationItem>
+    <NavigationItem link="/checkout">Checkout</NavigationItem>
   </ul>
 );
- 
-
 
 export default navigationItems;
